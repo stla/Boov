d5a5db036ee23b33a872785f5787db4b3d0ba65e
stla <laurent_step@yahoo.fr>
2022-10-20 13:47:53 +0200

    imports from PolygonSoup

--- R/aaa.R
+++ R/aaa.R
@@ -1,3 +1,15 @@
 #' @useDynLib Boov, .registration=TRUE
 #' @importFrom Rcpp evalCpp
 NULL
+
+#' Objects imported from other packages
+#' @description These objects are imported from other packages.
+#'   Follow the links to their documentation:
+#'   \code{\link[PolygonSoup:toRGL]{toRGL}},
+#'   \code{\link[PolygonSoup:plotEdges]{plotEdges}}.
+#' @importFrom PolygonSoup toRGL plotEdges
+#' @export toRGL plotEdges
+#' @name Boov-imports
+#' @aliases toRGL plotEdges
+#' @docType import
+NULL
