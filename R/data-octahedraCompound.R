eb24503d0c67f14b92f7c9943e140e2c2426ad4a
stla <laurent_step@yahoo.fr>
2022-10-20 21:08:26 +0200

    doc compounds

---
+++ R/data-octahedraCompound.R
@@ -0,0 +1,13 @@
+#' @title Compound of five octahedra
+#'
+#' @description Five octahedra in a pretty configuration. Each octahedron
+#'   is centered at the origin.
+#'
+#' @format A list with three fields: the field \code{meshes} is a list of five
+#'   elements, each one representing an octahedron by a list with two elements,
+#'   the vertices and the faces; the field \code{rglmeshes} is the list of
+#'   the five corresponding \strong{rgl} meshes; the field \code{gmpmeshes} is
+#'   the same as \code{meshes} except that the vertices are \strong{gmp}
+#'   rational numbers.
+#'
+"octahedraCompound"
